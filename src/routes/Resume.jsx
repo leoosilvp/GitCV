c7d9e6e85f730c8593b8bb73aaef5b1c35dee425
leeosilvp <leo965679@gmail.com>
2026-07-18 21:07:57 -0300

    feat: iniciando estrutura do curriculo em Resume

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,13 +1,50 @@
+import { Location, LogoGithub, LogoLinkedin } from '@carbon/icons-react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../css/resume.css'
+import { useUser } from '../hooks/useUser'
+import { Link } from 'react-router-dom'
+import ContributionPanel from '../components/ContributionPanel'
 
 const Resume = () => {
+
+    const { user } = useUser()
+
     return (
         <main className='resume-main'>
             <Header path='Resume' />
             <section className='resume-content'>
+                <article className='resume-view'>
+                    <header className='resume-header'>
+                        <div className='resume-header-info'>
+                            <img src={user?.avatar} />
+                            <div>
+                                <h1>{user?.name}</h1>
+                                <h2>Software Engieneer</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis aperiam in nemo facere, blanditiis reprehenderit fugiat distinctio odit error corporis sunt aliquid earum corrupti. Dolor obcaecati totam eaque labore velit.</p>
+                            </div>
+                        </div>
 
+                        <div className='resume-header-links'>
+                            <Link to={`https://github.com/${user?.username}`}><LogoGithub size={17} />{`github.com/${user?.username}`}</Link>
+                            <p>|</p>
+                            <Link><LogoLinkedin size={17} />{`linkedin.com/in/leonardo-silva`}</Link>
+                            <p>|</p>
+                            <Link><Location size={16} />leonardo.dev</Link>
+                            <p>|</p>
+                            <Link><Location size={16} />São Paulo, SP, Brasil</Link>
+                        </div>
+                    </header>
+
+                    <div className='resume-divider'>
+                        <h1>Contribution Activity (Last 12 months)</h1>
+                        <hr />
+                    </div>
+
+                    <section className='resume-contributionPanel'>
+                        <ContributionPanel isDownload />
+                    </section>
+                </article>
             </section>
             <Footer />
         </main>
