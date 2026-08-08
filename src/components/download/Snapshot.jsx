dfbdb0d74ae07107dc760bd7c69af51be55231b4
leeosilvp <leo965679@gmail.com>
2026-08-07 23:02:03 -0300

    feat: criando area com bg demarcando SnapshotCardBanner

--- src/components/download/Snapshot.jsx
+++ src/components/download/Snapshot.jsx
@@ -2,6 +2,7 @@ import SnapshotCard1 from '../snapshot/SnapshotCard1'
 import SnapshotCard2 from '../snapshot/SnapshotCard2'
 import SnapshotCard3 from '../snapshot/SnapshotCard3'
 import SnapshotCard4 from '../snapshot/SnapshotCard4'
+import SnapshotCardBanner from '../snapshot/SnapshotCardBanner'
 
 const Snapshot = () => {
   return (
@@ -12,6 +13,7 @@ const Snapshot = () => {
       </header>
 
       <section className='snapshot-grid-main'>
+        <SnapshotCardBanner />
         <SnapshotCard4 />
         <div className='snapshot-grid'>
           <SnapshotCard1 />
