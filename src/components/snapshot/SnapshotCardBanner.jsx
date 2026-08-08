051f688ccb1c0558500bbf95486cbe577e612663
leeosilvp <leo965679@gmail.com>
2026-08-07 23:16:54 -0300

    feat: criando conteudo de SnapshotCardBanner

--- src/components/snapshot/SnapshotCardBanner.jsx
+++ src/components/snapshot/SnapshotCardBanner.jsx
@@ -1,8 +1,19 @@
+import { LogoGithub } from "@carbon/icons-react"
+import { useUser } from "../../hooks/useUser"
 
 const SnapshotCardBanner = () => {
+
+    const { user } = useUser()
+
     return (
         <article className="snapshot-card-banner">
-            
+            <section>
+                <div>
+                    <h1>{user?.name}</h1>
+                    <hr />
+                    <p><LogoGithub size={16} />{user?.username}</p>
+                </div>
+            </section>
         </article>
     )
 }
