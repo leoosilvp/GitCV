6339fe496548d20e2c181b72934b3887f5e8549b
leeosilvp <leo965679@gmail.com>
2026-08-10 23:15:35 -0300

    feat: criando cards de features em Welcome

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,5 +1,5 @@
 import '../css/welcome.css'
-import { ArrowRight, Certificate, ChevronDown, Launch } from '@carbon/icons-react'
+import { ArrowRight, Certificate, ChevronDown, HeatMap_03, IbmKnowledgeCatalogPremium, Launch } from '@carbon/icons-react'
 import Header from '../components/welcome/Header'
 import { Link } from 'react-router-dom'
 
@@ -28,13 +28,33 @@ const Welcome = () => {
                         <p>Your career doesn't live inside a <span>PDF</span>. It lives in what you <span>build</span>, the problems you <span>solve</span>, and the technologies you <span>master</span>. GitCV brings it all together into a <span>dynamic professional identity</span> powered by your GitHub, transforming your activity into a profile that <span>tells your story</span>, showcases your <span>impact</span>, and evolves with you.</p>
                     </div>
                     <section className='welcome-features-grid'>
-                        <Link className='welcome-features-card'>
-                            <h1>Snapshots</h1>
+                        <Link to='/download/snapshots' className='welcome-features-card'>
+                            <h1>Git Snapshots</h1>
                             <h2>Download snapshots from your GitHub, share them on social media, and compare your progress with your friends.</h2>
 
                             <footer>
-                                <Certificate size={40} />
-                                <Launch size={20} />
+                                <Certificate className='icon' size={40} />
+                                <Launch className='icon' color='#57bf4f' size={20} />
+                            </footer>
+                        </Link>
+
+                        <Link to='/download/contributions' className='welcome-features-card'>
+                            <h1>Git Contributions</h1>
+                            <h2>Download your contribution graph, customize the theme, and share your progress.</h2>
+
+                            <footer>
+                                <HeatMap_03 className='icon' size={40} />
+                                <Launch className='icon' color='#57bf4f' size={20} />
+                            </footer>
+                        </Link>
+
+                        <Link to='/resume' className='welcome-features-card'>
+                            <h1>Git Résumé</h1>
+                            <h2>Download your PDF résumé featuring your key metrics, most used languages, top 3 projects, and GitHub highlights.</h2>
+
+                            <footer>
+                                <IbmKnowledgeCatalogPremium className='icon' size={40} />
+                                <Launch className='icon' color='#57bf4f' size={20} />
                             </footer>
                         </Link>
                     </section>
