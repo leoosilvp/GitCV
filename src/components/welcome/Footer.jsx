cd187fcc68178bbb176d5a75cbc035bcd80531d8
leeosilvp <leo965679@gmail.com>
2026-08-12 16:45:44 -0300

    feat: criando conteudo do footer de Welcome e adicionando links de redirecionamento

--- src/components/welcome/Footer.jsx
+++ src/components/welcome/Footer.jsx
@@ -10,41 +10,52 @@ const Footer = () => {
                     <p>&copy;2026 - All rights reserved</p>
                 </section>
                 <section className='welcome-footer-right'>
-                    <nav>
-                        <h1>Navigate</h1>
-                        <ul>
-                            <Link>Home</Link>
-                            <Link>Login</Link>
-                            <Link>Register</Link>
-                            <Link>About</Link>
-                            <Link>Features</Link>
-                            <Link>Support</Link>
-                        </ul>
-                    </nav>
+                    <div>
+                        <nav>
+                            <h1>Navigate</h1>
+                            <ul>
+                                <Link to='/home'>Home</Link>
+                                <Link to='/login'>Login</Link>
+                                <Link to='#about'>About</Link>
+                                <Link to='#support'>Support</Link>
+                            </ul>
+                        </nav>
 
-                    <nav>
-                        <h1>Navigate</h1>
-                        <ul>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                        </ul>
-                    </nav>
+                        <nav>
+                            <h1>GitCV</h1>
+                            <ul>
+                                <Link>GitHub</Link>
+                                <Link to='/contribute'>Contribute</Link>
+                                <Link to='#about'>About</Link>
+                            </ul>
+                        </nav>
 
-                    <nav>
-                        <h1>Navigate</h1>
-                        <ul>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                            <Link></Link>
-                        </ul>
-                    </nav>
+                        <nav />
+                    </div>
+
+                    <hr />
+
+                    <div>
+                        <nav>
+                            <ul>
+                                <Link to=''>Talk to GitCV</Link>
+                                <Link to=''>Privacy</Link>
+                            </ul>
+                        </nav>
+
+                        <nav>
+                            <ul>
+                                <Link to=''>Terms of Use</Link>
+                                <Link to=''>Accessibility</Link>
+                            </ul>
+                        </nav>
+
+                        <nav>
+                            <ul>
+                                <Link to=''>Cookie Preferences</Link>
+                            </ul>
+                        </nav>
+                    </div>
                 </section>
             </div>
         </footer>
